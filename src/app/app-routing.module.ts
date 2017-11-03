f20760bc2f4083e88228b60649faade1b0dc42a9
outofgamut <jsalvie@gmail.com>
2017-11-02 19:04:54 -0500

    add routing to talk structure, add home component, outline out presentation structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { RouterModule, Routes } from '@angular/router';
+import { PizzaOrderComponent } from './pizza/pizza-order/pizza-order.component';
+import { TalkComponent } from './talk/talk.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'talk', component: TalkComponent },
+  { path: 'pizza', component: PizzaOrderComponent },
+  { path: '**', component: HomeComponent } // always last
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatStepperModule } from '@angular/material';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -12,11 +13,14 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { PizzaModule } from './pizza/pizza.module';
 import { reducers } from './reducers';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { TalkComponent } from './talk/talk.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TalkComponent, HomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
@@ -25,6 +29,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     PizzaModule,
     MatToolbarModule,
     MatIconModule,
+    MatStepperModule,
     FlexLayoutModule
   ],
   bootstrap: [AppComponent]
